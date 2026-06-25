6daaa416fe9bf55e2e0e8f8df5f1ed1d6364e7c2
NehaJohn2808 <nunnaneha@gmail.com>
2026-06-25 12:12:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect
+import sqlite3
+
+app = Flask(__name__)
+
+# Create database
+def init_db():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            task TEXT NOT NULL
+        )
+    ''')
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route('/')
+def index():
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM tasks")
+    tasks = c.fetchall()
+
+    conn.close()
+
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form['task']
+
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+
+    c.execute("INSERT INTO tasks (task) VALUES (?)", (task,))
+    conn.commit()
+
+    conn.close()
+
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    conn = sqlite3.connect('todo.db')
+    c = conn.cursor()
+
+    c.execute("DELETE FROM tasks WHERE id=?", (id,))
+    conn.commit()
+
+    conn.close()
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
